e79d845b953c63edec4f4d0be13f84d59517198a
Willcreamson <101320403+Willcreamson@users.noreply.github.com>
2022-07-04 11:18:40 +0200

    Update BitcoinRate.jsx
    Final version of the Bitcoin Rate 
    Display the current value of bitcoin every minutes (60seconds)
    Comments added

--- src/BitcoinRate.jsx
+++ src/BitcoinRate.jsx
@@ -1,21 +1,24 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState} from 'react';
 
 function BitcoinRate(){
 
     const [bitcoinValue, setbitcoinValue] = useState("")
+    
+    // Asynchronous function that gets Bitcoun current rate value
     async function getBitCoin() {
         const response = await axios ({
             url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
             method: "GET"
         })
-
-        setbitcoinValue(response.data.bitcoin.usd)     
+        setInterval(() => {setbitcoinValue(response.data.bitcoin.usd)}, 60000);     // Update bitcoin rate value every 60 seconds
     }
     getBitCoin()
     console.log(bitcoinValue)
 
-    return <h1 style = { {position: 'absolute', top: '50%', left: '35%', textAlign:'center'} }> "{bitcoinValue} Bitcoin/USD " </h1>
+    // returns the current value of bitcoin in USD in the middle of the screen
+    return <h1 style = { {textAlign: 'center', justifyContent: 'center', position : 'relative', top:'250px'} }> "{bitcoinValue} Bitcoin/USD " </h1> 
 }
 
 export default BitcoinRate ;
+
